c32050a9089b4c0df73b8cfc25e418a967293eb2
Wessal Ayman <wessalayman237@gmail.com>
2024-03-24 03:49:18 +0200

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -1,75 +1,49 @@
 #!/usr/bin/python3
 """
-Module for the BaseModel class.
+Module for creating baseClass
 """
 import uuid
 from datetime import datetime
-#import models
+
 
 
 class BaseModel:
-	def __init__(self, *args, **kwargs):
-		time_format = "%Y-%m-%dT%H:%M:%S.%f"
+	def __init__(self):
 		self.id = str(uuid.uuid4())
-		self.created_at = datetime.utcnow()
-		self.updated_at = datetime.utcnow()
-		
-		if kwargs:
-			for key, value in kwargs.items():
-				if key == "__class__":
-					continue
-				elif key == "created_at" or key == "updated_at":
-					setattr(self, key, datetime.strptime(value, time_format))
-				else:
-					setattr(self, key, value)
-
-		#models.storage.new(self)
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
 
 	def save(self):
 		"""
-
+		Function that save
 		"""
-		self.updated_at = datetime.utcnow()
-		#models.storage.save()
+		self.updated_at = datetime.now()
 
 	def to_dict(self):
 		"""
-
+		function to 
 		"""
-		inst_dict = self.__dict__.copy()
-		inst_dict["__class__"] = self.__class__.__name__
-		inst_dict["created_at"] = self.created_at.isoformat()
-		inst_dict["updated_at"] = self.updated_at.isoformat()
-
-		return inst_dict
-
+		ob_dic = self.__dict__.copy()
+		ob_dic["__class__"] = self.__class__.__name__
+		ob_dic["created_at"] = self.created_at.isoformat()
+		ob_dic["updated_at"] = self.updated_at.isoformat()
+		return ob_dic
 	def __str__(self):
 		"""
-
+		Function that string represent
 		"""
-		class_name = self.__class__.__name__
-		return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-
+		className = self.__class__.__name__
+		return "[{}] ({}) {}".format(className, self.id, self.__dict__)
 
 if __name__ == "__main__":
 	my_model = BaseModel()
-	my_model.name = "My_First_Model"
+	my_model.name = "My First Model"
 	my_model.my_number = 89
-	print(my_model.id)
 	print(my_model)
-	print(type(my_model.created_at))
-	print("--")
+	my_model.save()
+	print(my_model)
 	my_model_json = my_model.to_dict()
 	print(my_model_json)
 	print("JSON of my_model:")
 	for key in my_model_json.keys():
 		print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-	# print("--")
-	# my_new_model = BaseModel(**my_model_json)
-	# print(my_new_model.id)
-	# print(my_new_model)
-	# print(type(my_new_model.created_at))
-
-	# print("--")
-	# print(my_model is my_new_model)
--- tests/test_model/test_base_model.py
+++ tests/test_model/test_base_model.py
@@ -7,82 +7,81 @@ import unittest
 from models.base_model import BaseModel
 
 
-
 class TestBasemodel(unittest.TestCase):
-	"""
-	Unittest for BaseModel
-	"""
+    """
+    Unittest for BaseModel
+    """
 
-	def setUp(self):
-		"""
-		Setup for temporary file path
-		"""
-		try:
-			os.rename("file.json", "tmp.json")
-		except FileNotFoundError:
-			pass
+    def setUp(self):
+        """
+        Setup for temporary file path
+        """
+        try:
+            os.rename("file.json", "tmp.json")
+        except FileNotFoundError:
+            pass
 
-	def tearDown(self):
-		"""
-		temporary
-		"""
-		try:
-			os.remove("file.json")
-		except FileNotFoundError:
-			pass
-		try:
-			os.rename("tmp.json", "file.json")
-		except FileNotFoundError:
-			pass
-	def test_init(self):
-		"""
-		init test
-		"""
-		my_model = BaseModel()
+    def tearDown(self):
+        """
+        temporary
+        """
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+        try:
+            os.rename("tmp.json", "file.json")
+        except FileNotFoundError:
+            pass
 
-		self.assertIsNotNone(my_model.id)
-		self.assertIsNotNone(my_model.created_at)
-		self.assertIsNotNone(my_model.updated_at)
+    def test_init(self):
+        """
+        init test
+        """
+        my_model = BaseModel()
 
-	def test_save(self):
-		"""
-		save test
-		"""
-		my_model = BaseModel()
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
 
-		initial_updated_at = my_model.updated_at
+    def test_save(self):
+        """
+        save test
+        """
+        my_model = BaseModel()
 
-		current_updated_at = my_model.save()
+        initial_updated_at = my_model.updated_at
 
-		self.assertNotEqual(initial_updated_at, current_updated_at)
+        current_updated_at = my_model.save()
 
-	def test_to_dict(self):
-		"""
-		to_dict test
-		"""
-		my_model = BaseModel()
+        self.assertNotEqual(initial_updated_at, current_updated_at)
 
-		my_model_dict = my_model.to_dict()
+    def test_to_dict(self):
+        """
+        to_dict test
+        """
+        my_model = BaseModel()
 
-		self.assertIsInstance(my_model_dict, dict)
+        my_model_dict = my_model.to_dict()
 
-		self.assertEqual(my_model_dict["__class__"], 'BaseModel')
-		self.assertEqual(my_model_dict['id'], my_model.id)
-		self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
+        self.assertIsInstance(my_model_dict, dict)
+        self.assertEqual(my_model_dict["__class__"], 'BaseModel')
+        self.assertEqual(my_model_dict['id'], my_model.id)
+        self.assertEqual(my_model_dict['created_at'],
+                         my_model.created_at.isoformat())
 
+    def test_str(self):
+        """
+        str represntation test
+        """
+        my_model = BaseModel()
 
-	def test_str(self):
-		"""
-		str represntation test
-		"""
-		my_model = BaseModel()
+        self.assertTrue(str(my_model).startswith('[BaseModel]'))
 
-		self.assertTrue(str(my_model).startswith('[BaseModel]'))
+        self.assertIn(my_model.id, str(my_model))
 
-		self.assertIn(my_model.id, str(my_model))
-
-		self.assertIn(str(my_model.__dict__), str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
 
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
