14506e685cde745cd51be59966acd46b693a948e
Wessal Ayman <wessalayman237@gmail.com>
2024-03-24 04:07:07 +0200

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,6 @@ if __name__ == "__main__":
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}"
               .format(key, type(my_model_json[key]), my_model_json[key]))
-        
     print("--")
     my_new_model = BaseModel(**my_model_json)
     print(my_new_model.id)
